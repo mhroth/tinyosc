df8fec8738cb087af2858a80fa995c3056740761
Thomas O Fredericks <tof@tofstuff.com>
2019-06-11 20:10:41 -0400

    Update TinyOsc.cpp

--- src/TinyOsc.cpp
+++ src/TinyOsc.cpp
@@ -218,7 +218,7 @@ size_t TinyOsc::vprint(Print* output, const char *address, const char *format, v
       }
       case 'f': {
         double  d = (double) va_arg(ap, double);//const float v = (float) va_arg(ap, double);
-        float v = d; // double might be 64 bytes
+        float v = d; // double might be 64 bites
         float v32 = tosc_bigEndian(v);
         uint8_t * ptr = (uint8_t *) &v32;
         //uint8_t * ptr = (uint8_t *) &v;
